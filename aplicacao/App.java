d537017c5f3be664f2ee61f89be9e9d5e8a5fbd3
GustaRD02 <gustavoramostere@gmail.com>
2023-06-13 21:36:45 -0300

    new commit

--- aplicacao/App.java
+++ aplicacao/App.java
@@ -1,7 +1,18 @@
 package aplicacao;
 
+import model.Pizza;
+
 public class App {
     public static void main(String[] args) {
-        
+        Pizza p = new Pizza("Frango", 30, 'p');
+
+        System.out.println("Pedido criado!!!");
+        System.out.println(p.pedidoIfood());
+
+        Pizza p2 = new Pizza("Camarão", 40, 'm');
+
+        System.out.println("Pedido criado!!!");
+        System.out.println(p2.pedidoIfood());
+
     }
 }
